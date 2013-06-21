eede957c8a2b84675de7fe981aa9b1d50910d234
Hugo <hwainwright@gmx.com>
2013-06-21 12:43:41 +0100

    - First commit!

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   Main.cpp
+ * Author: hugo
+ *
+ * Created on 21 June 2013, 12:34
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
